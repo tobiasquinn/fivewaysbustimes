07f2483c2d33528da18e1013dcee92be2770c72e
Tobias Quinn <tobias@tobiasquinn.com>
2011-01-27 02:16:28 +0000

    populate table from real data

--- fivewaysbustimes.py
+++ fivewaysbustimes.py
@@ -16,12 +16,20 @@ class Main(QtGui.QMainWindow):
         self.ui.setupUi(self)
 
         self.connect(self.ui.pushButtonRefresh, QtCore.SIGNAL('clicked()'), self._refreshData)
-        self._bd = BusData()
+        self._bd = BusData("service.urls")
 
     def _refreshData(self):
         print("Refreshing data")
         data = self._bd.getData()
         self.ui.tableWidgetTimes.setRowCount(len(data))
+        for i in range(len(data)):
+            twiBusnum = QtGui.QTableWidgetItem(data[i][0])
+            twiDest   = QtGui.QTableWidgetItem(data[i][1])
+            twiArrive = QtGui.QTableWidgetItem(data[i][2].strftime("%H:%M"))
+
+            self.ui.tableWidgetTimes.setItem(i, 1, twiBusnum)
+            self.ui.tableWidgetTimes.setItem(i, 2, twiDest)
+            self.ui.tableWidgetTimes.setItem(i, 0, twiArrive)
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
